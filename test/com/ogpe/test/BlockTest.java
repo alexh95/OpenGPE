c64bd5c440b42c3402cc0cac8e3b0429ed41a2dd
alexh95 <alexhuruiala@gmail.com>
2017-04-16 18:09:23 +0300

    Constant Block Fixes and Tests
    * Added tests for all ConstantBlock classes now using the ConstantBlock instead of manually making ConstnatBlockBehavior instances
    * fixed a typo in ConstantStringBlock

--- test/com/ogpe/test/BlockTest.java
+++ test/com/ogpe/test/BlockTest.java
@@ -5,19 +5,39 @@ import java.math.BigDecimal;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.ogpe.block.behaviour.implementation.ConstantBlockBehavior;
-import com.ogpe.block.behaviour.implementation.ConstantNumberBlockBehavior;
+import com.ogpe.block.implementation.ConstantBooleanBlock;
+import com.ogpe.block.implementation.ConstantNumberBlock;
+import com.ogpe.block.implementation.ConstantStringBlock;
+import com.ogpe.block.model.implementation.ConstantBooleanBlockModel;
 import com.ogpe.block.model.implementation.ConstantNumberBlockModel;
+import com.ogpe.block.model.implementation.ConstantStringBlockModel;
 
 public class BlockTest {
 
 	@Test
-	public void testConstantBlock() {
-		BigDecimal constantValue = BigDecimal.valueOf(5);
-		ConstantNumberBlockModel constantBlockModel = new ConstantNumberBlockModel(constantValue);
-		ConstantBlockBehavior<BigDecimal> constantBlock = new ConstantNumberBlockBehavior();
-		constantBlock.setBlockModelRequester(() -> constantBlockModel);
-		BigDecimal returnedValue = constantBlock.getOutputRequester().request();
-		Assert.assertEquals(constantValue, returnedValue);
+	public void testConstantNumberBlock() {
+		BigDecimal constantNumberValue = BigDecimal.valueOf(5);
+		ConstantNumberBlockModel constantNumberBlockModel = new ConstantNumberBlockModel(constantNumberValue);
+		ConstantNumberBlock constantNumberBlock = new ConstantNumberBlock(constantNumberBlockModel, 0, 0);
+		BigDecimal returnedValue = constantNumberBlock.getBlockBehavior().getOutputRequester().request();
+		Assert.assertEquals(constantNumberValue, returnedValue);
+	}
+
+	@Test
+	public void testConstantBooleanBlock() {
+		Boolean constantBooleanValue = true;
+		ConstantBooleanBlockModel constantBooleanBlockModel = new ConstantBooleanBlockModel(constantBooleanValue);
+		ConstantBooleanBlock constantBooleanBlock = new ConstantBooleanBlock(constantBooleanBlockModel, 0, 0);
+		Boolean returnedValue = constantBooleanBlock.getBlockBehavior().getOutputRequester().request();
+		Assert.assertEquals(constantBooleanValue, returnedValue);
+	}
+
+	@Test
+	public void testConstantStringBlock() {
+		String constantStringValue = "test string";
+		ConstantStringBlockModel constantStringBlockModel = new ConstantStringBlockModel(constantStringValue);
+		ConstantStringBlock constantStringBlock = new ConstantStringBlock(constantStringBlockModel, 0, 0);
+		String returnedValue = constantStringBlock.getBlockBehavior().getOutputRequester().request();
+		Assert.assertEquals(constantStringValue, returnedValue);
 	}
 }
